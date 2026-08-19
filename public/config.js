85af77884c601e918c5a74226381251455b08d53
Sergiys23 <sergiy904@gmail.com>
2026-08-19 15:39:49 +0300

    Add files via upload

--- public/config.js
+++ public/config.js
@@ -1,1 +1,1 @@
-window.MEHANIK_CONFIG={phone:'+380505944597',phoneDisplay:'+380 50 594 4597',address:'Решетилівська 53, Полтава',instagram:'https://www.instagram.com/mehanik.plt/',workingDays:[1,2,3,4,5,6]};
+window.MEHANIK_CONFIG={phone:"+380505944597",phoneDisplay:"+380 50 594 4597",address:"Решетилівська 53, Полтава",instagram:"https://www.instagram.com/mehanik.plt/",workingDays:[1,2,3,4,5,6]};
